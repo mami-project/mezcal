21d6280d8c1c1227f8c0f341ebe358e9366a9959
Brian Trammell <brian@trammell.ch>
2019-01-21 16:41:20 +0100

    script to upload using curl

---
+++ upload_curl.sh
@@ -0,0 +1,51 @@
+BASEURL=https://mami.cloudlab.zhaw.ch/mami/pto
+
+CAMPAIGN=$1
+BASEFILE=$2
+
+if [ ! -f ${BASEFILE} ]; then
+   echo data file ${BASEFILE} missing
+   exit 1
+fi
+
+METAFILE=${BASEFILE}.meta.ndjson
+if [ ! -f ${METAFILE} ]; then
+   echo metadata file ${METAFILE} missing
+   exit 1
+fi
+
+echo Compressing ${BASEFILE}...
+bzip2 $BASEFILE
+BZFILE=${BASEFILE}.bz2
+if [ ! -f ${METAFILE} ]; then
+   echo failed to compress: ${BZFILE} missing
+   exit 1
+fi
+
+echo "uploading metadata..."
+
+curl -X PUT \
+    -H "Authorization: APIKEY `cat token`" \
+    -H "Content-type: application/json" \
+    ${BASEURL}/raw/${CAMPAIGN}/${BZFILE} \
+    --data-binary @${METAFILE} | jq
+
+rv=$?
+if [ $rv -ne 0 ]; then
+    echo "failed to upload metadata, aborting"
+    exit $rv
+fi
+
+echo "uploading data..."
+
+curl -X PUT \
+    -H "Authorization: APIKEY `cat token`" \
+    -H "Content-type: application/bzip2" \
+    ${BASEURL}/raw/${CAMPAIGN}/${BZFILE}/data \
+    --data-binary @${BZFILE} | jq
+
+rv=$?
+if [ $rv -ne 0 ]; then
+    echo "failed to upload data, aborting"
+    exit $rv
+fi
